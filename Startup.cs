d8d72a9f36d26483726ba3113c1158398d4b2931
Manisha <41413650+ManishaThiruMurugan@users.noreply.github.com>
2024-05-20 02:51:44 +0530

    Required files

---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿// Startup.cs
+using Microsoft.OpenApi.Models;
+using OrderApi.Data;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("OrderList"));
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "OrderApi", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrderApi v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
